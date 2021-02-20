b653132f13d886276eedd1b98ffcd84e4aee3b61
kingBook <412126604@qq.com>
2021-02-20 17:29:30 +0800

    create

---
+++ Assets/SliceTest.cs
@@ -0,0 +1,23 @@
+﻿using EzySlice;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceTest : MonoBehaviour
+{
+    public GameObject sourceGo;//切割的物体
+    public GameObject slicerGo;//切片物体
+    public Material sectionMat;//切面材质
+ 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SlicedHull hull = sourceGo.Slice(slicerGo.transform.position, slicerGo.transform.up);
+            GameObject upper = hull.CreateUpperHull(sourceGo, sectionMat);
+            GameObject lower = hull.CreateLowerHull(sourceGo, sectionMat);
+            sourceGo.SetActive(false);
+        }
+    }
+
+}
